582bb7f05b509b7cb443fa0e956f46e19daf7807
Keith Marshall <keith@users.osdn.me>
2023-06-22 00:02:22 +0100

    Make <winber.h> header effectively self-contained.

--- w32api/include/winber.h
+++ w32api/include/winber.h
@@ -1,67 +1,89 @@
 /*
-  winber.h - Header file for the Windows LDAP Basic Encoding Rules API
-
-  Written by Filip Navara <xnavara@volny.cz>
-
-  References:
-    The C LDAP Application Program Interface
-    http://www.watersprings.org/pub/id/draft-ietf-ldapext-ldap-c-api-05.txt
-
-    Lightweight Directory Access Protocol Reference
-    http://msdn.microsoft.com/library/en-us/netdir/ldap/ldap_reference.asp
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-*/
-
+ * winber.h
+ *
+ * Windows LDAP Basic Encoding Rules API Declarations
+ *
+ * $Id$
+ *
+ * Written by Filip Navara <xnavara@volny.cz>
+ * Copyright (C) 2003, 2023, MinGW.OSDN Project
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ *
+ *
+ * References:
+ *   The C LDAP Application Program Interface
+ *   http://www.watersprings.org/pub/id/draft-ietf-ldapext-ldap-c-api-05.txt
+ *
+ *   Lightweight Directory Access Protocol Reference
+ *   http://msdn.microsoft.com/library/en-us/netdir/ldap/ldap_reference.asp
+ *
+ */
 #ifndef _WINBER_H
-#define _WINBER_H
-#if __GNUC__ >= 3
 #pragma GCC system_header
-#endif
+#define _WINBER_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <windef.h>
+
+_BEGIN_C_DECLS
 
 #ifndef WINBERAPI
 #define WINBERAPI DECLSPEC_IMPORT
 #endif
 
 typedef struct berelement BerElement;
-typedef ULONG ber_len_t;
-#include <pshpack4.h>
-typedef struct berval {
-	ber_len_t bv_len;
-	char *bv_val;
+typedef ULONG  ber_len_t;
+
+#pragma pack (push, 4)
+
+typedef struct berval
+{ ber_len_t	 bv_len;
+  char		*bv_val;
 } BerValue, LDAP_BERVAL, *PLDAP_BERVAL, BERVAL, *PBERVAL;
-#include <poppack.h>
 
-typedef ULONG ber_tag_t;
-typedef INT ber_int_t;
-typedef UINT ber_uint_t;
-typedef INT ber_slen_t;
+#pragma pack (pop)
 
-#define LBER_ERROR ((ber_tag_t)-1)
-#define LBER_DEFAULT ((ber_tag_t)-1)
-#define LBER_USE_DER 0x01
+typedef ULONG  ber_tag_t;
+typedef INT    ber_int_t;
+typedef UINT   ber_uint_t;
+typedef INT    ber_slen_t;
 
-WINBERAPI BerElement *ber_init(const BerValue*);
-WINBERAPI int ber_printf(BerElement*,const char*,...);
-WINBERAPI int ber_flatten(BerElement*,BerValue**);
-WINBERAPI ber_tag_t ber_scanf(BerElement*,const char*,...);
-WINBERAPI ber_tag_t ber_peek_tag(BerElement*,ber_len_t*);
-WINBERAPI ber_tag_t ber_skip_tag(BerElement*,ber_len_t*);
-WINBERAPI ber_tag_t ber_first_element(BerElement*,ber_len_t*,char**);
-WINBERAPI ber_tag_t ber_next_element(BerElement*,ber_len_t*,char*);
-WINBERAPI void ber_bvfree(BerValue*);
-WINBERAPI void ber_bvecfree(BerValue**);
-WINBERAPI void ber_free(BerElement*,int);
-WINBERAPI BerValue *ber_bvdup(BerValue*);
-WINBERAPI BerElement *ber_alloc_t(int);
+#define LBER_ERROR	((ber_tag_t)(-1))
+#define LBER_DEFAULT	((ber_tag_t)(-1))
+#define LBER_USE_DER	  0x01
 
-#ifdef __cplusplus
-}
-#endif
-#endif /* _WINBER_H */
+WINBERAPI BerElement *ber_init (const BerValue *);
+WINBERAPI int ber_printf (BerElement *, const char *, ...);
+WINBERAPI int ber_flatten (BerElement *, BerValue **);
+WINBERAPI ber_tag_t ber_scanf (BerElement *, const char *, ...);
+WINBERAPI ber_tag_t ber_peek_tag (BerElement *, ber_len_t *);
+WINBERAPI ber_tag_t ber_skip_tag (BerElement *, ber_len_t *);
+WINBERAPI ber_tag_t ber_first_element (BerElement *, ber_len_t *, char **);
+WINBERAPI ber_tag_t ber_next_element (BerElement *, ber_len_t *, char *);
+WINBERAPI void ber_bvfree (BerValue *);
+WINBERAPI void ber_bvecfree (BerValue **);
+WINBERAPI void ber_free (BerElement *, int);
+WINBERAPI BerValue *ber_bvdup (BerValue *);
+WINBERAPI BerElement *ber_alloc_t (int);
+
+_END_C_DECLS
+
+#endif /* !_WINBER_H: $RCSfile$: end of file */
